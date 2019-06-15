c4dc3e35f93cdc55146672f0dee024ec9f9e66bf
Samuel <samuelgarciacompanys@gmail.com>
2019-06-16 00:42:09 +0200

    added saving the data to a file

--- lib/scorer.rb
+++ lib/scorer.rb
@@ -26,7 +26,7 @@ class Scorer
     def round_remaining_time
         puts "The round will finish in..."
         0.downto(0) do |i|
-            puts "00:00:#{'%02d' % i}"
+            puts "00:#{'%02d' % i}"
             sleep 1
           end
     end
@@ -48,6 +48,18 @@ class Scorer
         end
         bm.rounds[round_counter].fighter1_scored = f1_points
         bm.rounds[round_counter].fighter2_scored = f2_points
+
+        write_in_file(bm, round_counter, f1_points, f2_points)
+    end
+
+    def write_in_file(bm, round_counter, f1_points, f2_points)
+        bm_name = bm.fighter1.to_s + "_vs_" + bm.fighter2.to_s
+        file = File.open('../data/scoring_'+ bm_name +'.txt', 'a')
+        lines = bm.fighter1 + "," + bm.fighter2
+        file.puts bm.fighter1 + "," + bm.fighter2 if round_counter == 1
+        lines = f1_points.to_s + "," + f2_points.to_s
+        file.puts lines
+        file.close
     end
 
 
